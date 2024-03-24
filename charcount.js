98294d0654c2973282feafc3fae67d0a6a489f73
Carter Clifton <101024013+carter-clifton@users.noreply.github.com>
2024-03-24 18:42:57 -0300

    Add files via upload

--- charcount.js
+++ charcount.js
@@ -2,4 +2,9 @@ function updateCharacters() {
   let l = document.getElementById("charArea").value.length;
   console.log(l);
   document.getElementById('chars').innerHTML = l;
+  if (l >= 1000) {
+    document.getElementById("charArea").style.backgroundColor = "red";
+  } else {
+    document.getElementById("charArea").style.backgroundColor = "#101a24"
+  }
 }
