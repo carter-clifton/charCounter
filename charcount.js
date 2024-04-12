ee4e20ee82e038307f8b14a3dbbbe6642bfb0adb
Carter Clifton <101024013+carter-clifton@users.noreply.github.com>
2024-04-12 15:38:32 -0300

    updating all the files

--- charcount.js
+++ charcount.js
@@ -1,10 +1,13 @@
 function updateCharacters() {
   let l = document.getElementById("charArea").value.length;
-  console.log(l);
   document.getElementById('chars').innerHTML = l;
-  if (l >= 1000) {
-    document.getElementById("charArea").style.backgroundColor = "red";
+  if (l >= parseInt(document.getElementById("characterLimitInput").value)) {
+    document.getElementById('body').style.backgroundColor = "#f44336";
+    document.getElementById('charArea').style.backgroundColor = "#b71c1c";
+    document.getElementById('characterLimitInput').style.backgroundColor = "#b71c1c"
   } else {
-    document.getElementById("charArea").style.backgroundColor = "#101a24"
+    document.getElementById('body').style.backgroundColor = "#15222e";
+    document.getElementById('charArea').style.backgroundColor = "#101a24";
+    document.getElementById('characterLimitInput').style.backgroundColor = "#101a24"
   }
 }
